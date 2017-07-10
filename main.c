d963103c5be74d1ef8f93b821f426d7d0454f4b5
luisejrobles <luisejimenezrobles@gmail.com>
2017-07-10 16:40:23 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Cronometro.c
+ *
+ * Created: 10/07/2017 04:39:37 p. m.
+ * Author : LuisEduardo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
